511c28a319a8501ce04735207a5b33bd4cf9f027
KD1024 <dk@Kes-MacBook-Pro.local>
2017-05-10 13:10:24 -0400

    LPR book test after chapter 4

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,13 +3,48 @@ import ReactDOM from 'react-dom';
 
 var HelloWorld = React.createClass({
   render: function(){
+    var buttonLabel = "Submit";
+    var isValid = false;
     return (
-      <div>Hello World!</div>
+      <div>
+        <Hello/><World/>! <br/>
+        <button>{buttonLabel}</button>
+        {/*The JS in brace must be an expression
+          not a statement! */}
+        <div>
+          <span>{isValid ? 'Valid' : 'not Valid'}</span><br/>
+          <span>
+            {isValid && 'valid'}
+            {!isValid && 'not valid'}
+          </span>
+        </div>
+      </div>
     );
   }
 });
+
+var Hello = React.createClass({
+  render: function(){
+    return (
+      // This is actually the JSX
+      <span>Hello</span>
+    );
+  }
+});
+
+var World = React.createClass({
+  render: function(){
+    return (
+      // This is actually the JSX
+      <span>World</span>
+    );
+  }
+});
+
+
 
 ReactDOM.render(
   <HelloWorld/>,
   document.querySelector('#root')
 );
+// ReactDOM.render([React Element], [DOM Element]);
