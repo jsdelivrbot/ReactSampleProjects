fd625a207ffa7e50d9e58ffd9034fa40b3d829ed
KD1024 <dk@Kes-MacBook-Pro.local>
2017-05-25 11:23:10 -0400

    change for the small example

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 // Create a new component
 // This component should produce some html
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return (<div>Hi!</div>);
 }
 
 // Take this component's generated HTML and
 // put it on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
